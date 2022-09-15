95cbdb1b702550d2f0334e7a1dd4921f39232df2
jdavis <joedavis29@gmail.com>
2022-09-15 10:35:34 -0400

    reverting

--- src/client/metrics-query.ts
+++ src/client/metrics-query.ts
@@ -152,7 +152,6 @@ export const metricsQuery = (items: SmallItems | null | undefined, target: GpeTa
     const transient_max = i.transient ? i.transient.length : 0
     l = Math.max(metrics_max, configs_max, transient_max)
     l = l == 0? 1 : l
-    console.log(l)
 
     const fields: FieldDTO<any>[] = [
       ...valueToField(i.id, "item_id", l),
@@ -163,7 +162,7 @@ export const metricsQuery = (items: SmallItems | null | undefined, target: GpeTa
       fields.push(valueToField(target.custom_tags[0], "custom_tag", l)[0])
     }
 
-    if (i.metrics.length === 0 && i.configs.length === 0 && i.transient && i.transient.length === 0)  {
+    if (i.metrics.length === 0 && i.configs.length === 0 &&  i.transient && i.transient.length === 0)  {
       const frame = new MutableDataFrame({
         name: `${i.label}_${i.id}`,
         fields,
