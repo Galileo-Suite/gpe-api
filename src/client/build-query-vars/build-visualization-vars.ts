419eb14c5aaa1e52aec1da23d13af52aa8b577c7
jdavis <joedavis29@gmail.com>
2022-10-26 16:42:27 -0400

    add templating for summary

--- src/client/build-query-vars/build-visualization-vars.ts
+++ src/client/build-query-vars/build-visualization-vars.ts
@@ -1,24 +1,21 @@
 import { GpeQuery } from '../../types/query';
 import { VisualizationQueryVariables } from '../queries/queries';
 import { buildSelectorVars } from './build-selector-vars'
+import { buildSummaryVars } from './build-summary-vars'
 
 export const buildVisualizationVars = (
   target: Partial<GpeQuery>,
   { epoch_start, epoch_end }: { epoch_start: number, epoch_end:number }
 ): VisualizationQueryVariables => {
   const {
-    summary,
-    samples,
-
     vis_id,
     filters,
   } = target;
 
   let vars: VisualizationQueryVariables = {
     ...buildSelectorVars(target),
+    ...buildSummaryVars(target),
 
-    summary,
-    samples,
     vis_id: (vis_id ?? [""])[0],
     filters,
     function: target.function, // doing this cuz function is reserved
@@ -26,10 +23,6 @@ export const buildVisualizationVars = (
     epoch_start,
     epoch_end,
   };
-
-  if (vars.samples) {
-    vars.summary = null;
-  }
 
   return vars;
 };
