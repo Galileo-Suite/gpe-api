1c7536e7d1d45d7db4cfd117f9e00a14a2343cc9
jdavis <joedavis29@gmail.com>
2022-11-07 15:37:02 -0500

    Revert "sync configs with transients"
    This reverts commit f5cb03fcf874c3a603f06eba0952c4d3f641633f.

--- src/client/to-dataframes/transients-to-dataframes.ts
+++ src/client/to-dataframes/transients-to-dataframes.ts
@@ -1,6 +1,6 @@
 
 import { TransientsDocument } from '../queries/queries'
-import { MutableDataFrame, FieldType, FieldDTO, FieldConfig } from '@grafana/data';
+import { MutableDataFrame, FieldType, FieldDTO } from '@grafana/data';
 
 import {ResultOf} from '@graphql-typed-document-node/core'
 import { GpeQuery } from '../../types';
@@ -12,20 +12,13 @@ type Unarray<T> = T extends Array<infer U> ? U : T;
 type SmallTransients = Unarray<SmallItems>['transient']
 type SmallConfigs = Unarray<SmallItems>['configs']
 
-interface Field<T> {
-  name: string;
-  type?: FieldType;
-  config?: FieldConfig;
-  values?: T[];
-}
-
-const itemToMetricFields = (metrics: SmallTransients, l = 1, prefix=""): Field<number | null>[] => {
+const itemToMetricFields = (metrics: SmallTransients, l = 1, prefix=""): FieldDTO<number | null>[] => {
   if (metrics.length == 0 ) {
     return []
   }
 
   const frames: MutableDataFrame<any>[]  = []
-  const fields: Field<any>[] = []
+  const fields: FieldDTO<any>[] = []
   metrics.forEach(c=> {
     if (!c?.data) {
       return;
@@ -51,7 +44,7 @@ const itemToMetricFields = (metrics: SmallTransients, l = 1, prefix=""): Field<n
   return fields
 }
 
-const itemToConfigFields = (configs: SmallConfigs, l?: number, prefix: string= ""): Field<string | null>[] => {
+const itemToConfigFields = (configs: SmallConfigs, l?: number, prefix: string= ""): FieldDTO<string | null>[] => {
   if (configs.length == 0 ) {
     return []
   }
@@ -64,36 +57,6 @@ const itemToConfigFields = (configs: SmallConfigs, l?: number, prefix: string= "
       values,
       type: FieldType.string,
     }
-  })
-}
-
-const itemToConfigFieldsBasedOnTime = (configs: SmallConfigs, timeField: (number|null)[], prefix: string= ""): FieldDTO<string | null>[] => {
-  if (configs.length == 0 ) {
-    return []
-  }
-
-  return configs.map(m=>{
-    const values: (string | null)[] = []
-    const tuples = m.tuple?.sort((a,b) => b.epoch - a.epoch)
-    if (tuples) {
-      values.push(
-        ...timeField.map(t=>{
-          const tup = tuples.find(tup => {
-            if (tup.epoch <= (t??0)/1000) {
-              return true
-            }
-            return false
-          })
-          return tup?.value ?? null
-        })
-      )
-    }
-    return {
-      name: `${prefix? prefix+ '_' : ''}${m.field}`,
-      values,
-      type: FieldType.string,
-    }
-
   })
 }
 
@@ -115,22 +78,12 @@ export const transientsToDataFrames = (items: SmallItems | null | undefined, tar
       l = Math.max( transient_max, 1)
     }
 
-    const metricsFeilds = itemToMetricFields(i.transient, l, i.item_type)
-    const timefield = metricsFeilds.find(f=>f.type === FieldType.time)
+    fields.push(
+      ...valueToField(i.id, 'item_id', l),
+      ...itemToConfigFields(i.configs,   l, i.item_type),
+      ...itemToMetricFields(i.transient, l, i.item_type),
+      )
 
-    if (timefield === undefined) { //case when they dont requests metrics
-      fields.push(
-        ...valueToField(i.id, 'item_id', l),
-        ...itemToConfigFields(i.configs,   l, i.item_type),
-        ...metricsFeilds
-      )
-    } else {
-      fields.push(
-        ...valueToField(i.id, 'item_id', l),
-        ...itemToConfigFieldsBasedOnTime(i.configs, timefield.values ?? [], i.item_type),
-        ...metricsFeilds
-      )
-    }
     target.includedMetaData?.forEach(md=> {
       switch (md) {
         case 'refid': fields.push(...valueToField(target.refId, 'refid', l) ); break
